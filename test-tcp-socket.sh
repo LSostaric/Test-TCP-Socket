7459ae9718c7c4a21498bde0394b8691f798a304
Luka Sostaric <luka@lukasostaric.com>
2019-07-21 14:46:02 +0200

    Fixed indentations.

---
+++ test-tcp-socket.sh
@@ -0,0 +1,46 @@
+#!/bin/ksh -p
+if [ "$1" == "help" ] ; then
+    echo "  Usage: Test-TCP-Socket.ksh destination-addr destination-port source-addr expected-output [success-message] [fail-message]"
+    exit 0
+fi
+if [ -z "$1" ] ; then
+    echo "  The server address has to be specified as the first parameter. Terminating..."
+    echo "  Use Test-TCP-Socket.ksh help for usage."
+    exit 1
+elif [ -z "$2" ] ; then
+    echo "  You have to specify the port as the second parameter. Terminating..."
+    echo "  Use Test-TCP-Socket.ksh help for usage."
+    exit 2
+elif [ -z "$3" ] ; then
+    echo "  You have to specify the source address. Terminating..."
+    echo "  Use Test-TCP-Socket.ksh help for usage."
+    exit 3
+elif [ -z "$4" ] ; then
+    echo "  You have to specify the output expected from the socket."
+    echo "  Use Test-TCP-Socket.ksh help for usage"
+    exit 3
+fi
+host="$1"
+port="$2"
+source="$3"
+expected="$4"
+if [ -z "$6" ] ; then
+    fail="  Invalid output from $host on port $port."
+else
+    fail="$6"
+fi
+if [ -z "$5" ] ; then
+    success="  O.K. Success."
+else
+    success="$5"
+fi
+output="$(nc -s "$source" "$host" "$port" 2>&1 <<EOF
+EOF
+)"
+
+if [ $(echo "$output" | grep -ic "$expected") -eq 0 ] ; then
+        echo "$fail"
+    else
+        echo "$success"
+fi
+exit 0
